8e8d5e533108977d007f0a72b2655445b79b3bbb
Ueda Takeyuki <ueda@latelierdueda.com>
2018-06-14 22:29:13 +0900

    wss:

--- wc.py
+++ wc.py
@@ -1,10 +1,14 @@
 #-*- coding:utf-8 -*-
 
-from websocket import create_connection
+#from websocket import create_connection
+import websocket
 import sys
+import ssl
 
 #コネクションを張る
-ws = create_connection("ws://titurel.uedasoft.com:8888/websocket")
+#ws = create_connection("wss://titurel.uedasoft.com:8888/websocket")
+ws = websocket.WebSocket(sslopt={"cert_reqs": ssl.CERT_NONE})
+ws.connect("wss://titurel.uedasoft.com:8888/websocket")
  
 #メッセージを送信
 ws.send('hello world!')

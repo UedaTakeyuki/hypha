d4948fa282fda98762d5276e9644e5c6b4f74cf7
Ueda Takeyuki <ueda@latelierdueda.com>
2018-06-15 09:20:55 +0900

    command option

--- wc.py
+++ wc.py
@@ -4,17 +4,32 @@
 import websocket
 import sys
 import ssl
+from tornado.options import define, options
 
-#コネクションを張る
-#ws = create_connection("wss://titurel.uedasoft.com:8888/websocket")
-ws = websocket.WebSocket(sslopt={"cert_reqs": ssl.CERT_NONE})
-ws.connect("wss://titurel.uedasoft.com:8888/websocket")
- 
-#メッセージを送信
-ws.send('hello world!')
+if __name__ == "__main__":
 
-#受信したメッセージを表示
-print ws.recv()
+	# command options
+	define("protocol", default="wss:", help="ws: or wss:(default)")
+	define("url", default="//titurel.uedasoft.com:8888/websocket", help="url string without protocol like //aaa.bbb.ccc/ddd")
+	define("config_file", default="", help="config file path")
+	options.parse_command_line()
+
+	if options.config_file:
+		options.parse_config_file(options.config_file)
+
+	if options.protocol == "ws:":
+		#ws = create_connection("wss://titurel.uedasoft.com:8888/websocket")
+		ws = websocket.WebSocket()
+ 	else:
+		ws = websocket.WebSocket(sslopt={"cert_reqs": ssl.CERT_NONE})
+
+	ws.connect(options.protocol + options.url)
+
+	#メッセージを送信
+	ws.send('hello world!')
+
+	#受信したメッセージを表示
+	print ws.recv()
  
-#コネクションを切断
-ws.close()
+	#コネクションを切断
+	ws.close()
